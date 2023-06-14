3d9683d155f4035b65ac1f20506708227c0b2f4f
Jisun <117595989+Jisun-Lee@users.noreply.github.com>
2023-06-14 14:33:12 -0700

    Adds tie tally, function to clear out game results

--- main.js
+++ main.js
@@ -2,67 +2,92 @@ let playerSelection = '';
 let computerSelection = '';
 let playerTally = 0; 
 let computerTally = 0;
+let tieTally = 0;
 
+let buttons = document.querySelectorAll('.button');
+console.log(buttons);
 const buttonContainer = document.querySelector('.buttonContainer');
-const result = document.createElement('div');
-const rock = document.querySelector('.rock');
-const paper = document.querySelector('.paper');
-const scissors = document.querySelector('.scissors');
+const statusBox = document.querySelector('.statusBox');
+const round = document.querySelector('.round');
+const gameOver = document.querySelector('.gameOver');
 
-//puts result into buttonContainer
-buttonContainer.appendChild(result);
+const roundResults = document.createElement('div');
+const tally = document.createElement('div');
+const resultGame = document.createElement('div');
+const resetButton = document.createElement('button');
 
-//adds event listeners to each button
-rock.addEventListener("click", () => {
+//puts game results into .statusBox div
+round.appendChild(roundResults);
+round.appendChild(tally);
+gameOver.appendChild(resultGame);
+gameOver.appendChild(resetButton);
+
+//creates invisible reset button for later, when the game ends
+gameOver.style.display = 'none';
+resetButton.textContent = 'Wanna try again?';
+
+//adds event listeners to each button, run game functions
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const img = button.querySelector('img');
+      playerSelection = img.alt;
+  
+      playRound(img.alt);
+  
+      if (playerTally === 5 || computerTally === 5) {
+        gameStatus();
+      }
+    });
+  });
+
+/*rock.addEventListener("click", () => {
     playRound('rock');
-    result.textContent = 
-        `${roundStatus}
-        player wins: ${playerTally}, computer wins: ${computerTally}`
+    gameStatus();
     });
 paper.addEventListener("click", () => {
     playRound('paper');
-    result.textContent = 
-        `${roundStatus}
-        player wins: ${playerTally}, computer wins: ${computerTally}`
+    gameStatus();
     });
 scissors.addEventListener("click", () => {
     playRound('scissors');
-    result.textContent = 
-        `${roundStatus}
-        player wins: ${playerTally}, computer wins: ${computerTally}`
-    });
+    gameStatus();
+    });*/
+
 
 function playRound(playerSelection) {
-    computerSelection = getComputerSelection();
-    console.log(playerSelection);
-    console.log(computerSelection);
-    let winMsg = 
-        `You win! ${playerSelection.charAt(0).toUpperCase() + 
-            playerSelection.slice(1)} beats ${computerSelection}. 
-            Time for a victory lap.`;
-    let loseMsg = 
-        `Awwww, you lose. ${computerSelection.charAt(0).toUpperCase() + 
-            computerSelection.slice(1)} beats ${playerSelection}.`;
-    let tieMsg = 
+    computerSelection = getComputerSelection();   
+    if (playerTally >= 5 || computerTally >= 5) {
+        playerTally = 0;
+        computerTally = 0;
+        statusBox.style.display = 'none';
+    } else if 
+        ((playerSelection == 'rock') && (computerSelection == 'scissors') ||
+        (playerSelection == 'paper') && (computerSelection == 'rock') ||
+        (playerSelection == 'scissors') && (computerSelection == 'paper')) 
+        {
+            statusBox.style.display = 'block';
+            playerTally += 1;
+            roundResults.textContent = 
+            `You win! ${capitalize(playerSelection)} beats 
+            ${computerSelection}.`;
+    } else if 
+        ((playerSelection == 'rock') && (computerSelection == 'paper') ||
+        (playerSelection == 'paper') && (computerSelection == 'scissors') ||
+        (playerSelection == 'scissors') && (computerSelection == 'rock')) 
+        {
+            statusBox.style.display = 'block';
+            computerTally += 1;
+            roundResults.textContent = 
+            `Awwww, you lose. ${capitalize(computerSelection)} beats 
+            ${playerSelection}.`;
+    } else if (playerSelection === computerSelection) {
+        statusBox.style.display = 'block';
+        tieTally += 1;
+        roundResults.textContent = 
         `Tie! Try again. Deep breath, you can do this.`;
-        if 
-            ((playerSelection == 'rock') && (computerSelection == 'scissors') ||
-            (playerSelection == 'paper') && (computerSelection == 'rock') ||
-            (playerSelection == 'scissors') && (computerSelection == 'paper')) {
-                roundStatus = winMsg;
-                playerTally += 1;
-        } else if 
-            ((playerSelection == 'rock') && (computerSelection == 'paper') ||
-            (playerSelection == 'paper') && (computerSelection == 'scissors') ||
-            (playerSelection == 'scissors') && (computerSelection == 'rock')) {
-                roundStatus = loseMsg;
-                computerTally += 1;
-        } else {
-            roundStatus = tieMsg;
-        }
-    console.log(playerTally, computerTally);
-    result.textContent = roundStatus;
-    return roundStatus;
+    }
+    tally.textContent = 
+        `You: ${playerTally} Computer: ${computerTally} Tie: ${tieTally}`;
 }  
 
 //better as a switch statement?
@@ -77,7 +102,42 @@ function getComputerSelection() {
     }
 } 
 
-/*function playGame(playerSelection) {
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+function gameStatus() {
+    //clearScreen = () => {buttonContainer.style.display = 'none'};
+    if (playerTally === 5) {
+        statusMsg = 
+        'You won! Maybe there is hope for humanity, after all.';
+        gameOver.style.display = 'block';
+        gameEnd();
+    } else if (computerTally === 5) {
+        statusMsg = 
+        'It is crushing to have to tell you this, but you have lost to random chance.';
+        gameOver.style.display = 'block';
+        gameEnd();
+    } else {
+        statusMsg = 
+        'Keep going! You got this.';
+    }
+    resultGame.textContent = statusMsg;
+}
+
+//when do I need to reset the counter?
+//how to I erase the reset div content after the player starts playing again...?
+function gameEnd() {
+    resetButton.addEventListener('click', function() {
+        playerTally = 0;
+        computerTally = 0;
+
+        statusBox.style.display = 'none';
+    }, { once: true });
+
+}
+
+/*function playRound(playerSelection) {
     for (let counter = 5; counter >= 0; counter--) {
         playRound(playerSelection);
         if (playerSelection == computerSelection) {
